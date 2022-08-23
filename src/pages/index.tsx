3df777e58c44c8be0c34b406df83e034c21dbc65
Steven Kok <skok@cfp.nl>
2022-08-23 16:05:44 +0200

    rounder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
-
-export default function Home(){
-    return <div className="h-screen w-screen flex flex-col">hoi Jaap</div>
+export default function Home() {
+    return <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-2xl text-center">Which Pokémon is rounder?</div>
+        <div className="p-2"/>
+        <div className="border rounded p-8 flex justify-between max-w-2xl">
+            <div className="w-16 h-16 bg-red-200" ></div>
+            <div className="p-8">Vs</div>
+            <div className="w-16 h-16 bg-red-200" ></div>
+        </div>
+    </div>
 }
